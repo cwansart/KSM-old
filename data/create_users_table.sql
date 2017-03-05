a30db0c81529b212eef686b25bc8548640e9ea7a
Christian Wansart <cwansart@users.noreply.github.com>
2017-03-05 10:47:24 +0100

    Fixed sql structure

--- data/create_users_table.sql
+++ data/create_users_table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL PRIMARY KEY,
-  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_520_ci NOT NULL UNIQUE,
   `password` binary(120) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -10,6 +10,6 @@ INSERT INTO `users` (
     `password`
 ) VALUES (
     'root',
-    HEX('$2y$10$WDxPBjwAa5V8xcAk.PyZcuUUwx95dgESUsOgh5mS4tK.vYoRSwAuG')
+    CONCAT('0x', HEX('$2y$10$WDxPBjwAa5V8xcAk.PyZcuUUwx95dgESUsOgh5mS4tK.vYoRSwAuG'))
     -- Change this password!
 );
